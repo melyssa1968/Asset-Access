19af031413929e8ac1ab998512400e73b940f790
melyssa1968 <165384515+melyssa1968@users.noreply.github.com>
2026-09-05 16:55:53 -0400

    Match Clerk proxy to the API route

--- app/api/%5F%5Fclerk/[[...path]]/route.ts
+++ app/api/%5F%5Fclerk/[[...path]]/route.ts
@@ -1,4 +1,6 @@
 import { createFrontendApiProxyHandlers } from "@clerk/nextjs/server";
 
 export const { GET, POST, PUT, DELETE, PATCH } =
-  createFrontendApiProxyHandlers();
+  createFrontendApiProxyHandlers({
+    proxyPath: "/api/__clerk",
+  });
